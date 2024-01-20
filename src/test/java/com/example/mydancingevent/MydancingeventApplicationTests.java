48fc2ff29a6fa6ae211536caaed65bc85446d5f5
Oliver Zihler <ozihler@gmail.com>
2024-01-20 18:16:58 +0100

    init

---
+++ src/test/java/com/example/mydancingevent/MydancingeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydancingevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydancingeventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
