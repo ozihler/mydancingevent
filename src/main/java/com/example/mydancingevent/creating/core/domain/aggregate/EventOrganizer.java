9a8ef4009fff79c5e2780693ac7927e8fc84f7c0
Oliver Zihler <ozihler@gmail.com>
2024-01-21 09:16:38 +0100

    4.10

--- src/main/java/com/example/mydancingevent/creating/core/domain/aggregate/EventOrganizer.java
+++ src/main/java/com/example/mydancingevent/creating/core/domain/aggregate/EventOrganizer.java
@@ -2,13 +2,18 @@ package com.example.mydancingevent.creating.core.domain.aggregate;
 
 import com.example.mydancingevent.creating.core.domain.value.DancingEventId;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EventOrganizer {
     private final List<DancingEventId> unpublishedDancingEvents;
 
-    public EventOrganizer(List<DancingEventId> unpublishedDancingEvents) {
+    private EventOrganizer(List<DancingEventId> unpublishedDancingEvents) {
         this.unpublishedDancingEvents = unpublishedDancingEvents;
+    }
+
+    public static EventOrganizer create() {
+        return new EventOrganizer(new ArrayList<>());
     }
 
     public void addDancingEvent(DancingEventId dancingEventId) {
--- src/test/java/com/example/mydancingevent/creating/core/application/usecase/CreateDancingEventUseCaseShould.java
+++ src/test/java/com/example/mydancingevent/creating/core/application/usecase/CreateDancingEventUseCaseShould.java
@@ -7,7 +7,6 @@ import com.example.mydancingevent.creating.core.domain.value.DancingEventId;
 import com.example.mydancingevent.creating.core.domain.value.EventOrganizerId;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,7 +15,7 @@ public class CreateDancingEventUseCaseShould {
 
     @Test
     void fail_if_the_event_organizer_id_is_missing() {
-        var eventOrganizer = new EventOrganizer(new ArrayList<>());
+        var eventOrganizer = EventOrganizer.create();
 
         assertThrows(MissingEventOrganizerId.class, () -> {
             new CreateDancingEventUseCase(eventOrganizer).invoke(null);
@@ -33,17 +32,8 @@ public class CreateDancingEventUseCaseShould {
     }
 
     @Test
-    void add_a_dancing_event_to_an_event_organizer() throws Exception {
-        var eventOrganizer = new EventOrganizer(new ArrayList<>());
-
-        new CreateDancingEventUseCase(eventOrganizer).invoke(EventOrganizerId.create("EO-1"));
-
-        assertEquals(new DancingEventId("DE-1"), eventOrganizer.unpublishedDancingEvents().getFirst());
-    }
-
-    @Test
     void add_multiple_dancing_events_to_an_event_organizer() throws Exception {
-        var eventOrganizer = new EventOrganizer(new ArrayList<>());
+        var eventOrganizer = EventOrganizer.create();
 
         var useCase = new CreateDancingEventUseCase(eventOrganizer);
         useCase.invoke(EventOrganizerId.create("EO-1"));
